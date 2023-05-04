139278302c42797e5ab394908fa34450661c3b17
sakamoto051 <sakamoto051eng@gmail.com>
2023-05-04 09:48:22 +0900

    feat: character movement

---
+++ Source/Unknown/PlayerCharacter.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UnknownCharacter.h"
+#include "PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNKNOWN_API APlayerCharacter : public AUnknownCharacter
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+	void LookUpRate(float AxisValue);
+	void LookRightRate(float AxisValue);
+
+	UPROPERTY(EditAnywhere)
+	float RotationRate = 10;
+};
